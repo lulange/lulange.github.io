b8206a937ae1bd966aa8328acd6ae6efa77321ea
lulange <53273617+lulange@users.noreply.github.com>
2019-08-14 21:14:49 -0400

    Add files via upload

--- DrawingGame/script.js
+++ DrawingGame/script.js
@@ -8,24 +8,23 @@ var drawWidth = 35;
 
 var draw = function(x, y) {
   var divEl = document.createElement("div");
-  var divElSty = divEl.style;
-  divElSty.left = x-drawWidth/2 + "px";
-  divElSty.top = y-drawWidth/2 + "px";
-  divElSty.width = drawWidth + "px";
-  divElSty.height = drawWidth + "px";
+  divEl.style.left = x-drawWidth/2 + "px";
+  divEl.style.top = y-drawWidth/2 + "px";
+  divEl.style.width = drawWidth + "px";
+  divEl.style.height = drawWidth + "px";
   divEl.classList.add("drawer-divs", "yellow");
   document.body.appendChild(divEl);
 };
 
 var mouseMoveFunction = function(e) {
   if (isMouseDown === true) {
-    draw(e.clientX, e.clientY);
+    draw(e.pageX, e.pageY);
   }
 };
 
 var mouseDownFunction = function(e) {
   isMouseDown = true;
-  draw(e.clientX, e.clientY);
+  draw(e.pageX, e.pageY);
 };
 
 var mouseUpFunction = function(e) {
