9043ba3ef9a2a3c87ca2b1e0abfbeb25ed2f4e34
lulange <53273617+lulange@users.noreply.github.com>
2019-08-17 07:24:29 -0400

    Add files via upload

--- DrawingGame/script.js
+++ DrawingGame/script.js
@@ -6,7 +6,7 @@ document.body.style.width = window.innerWidth + "px";
 
 var isMouseDown = false;
 var drawWidth = 35;
-var drawColor = "blue";
+var drawColor = "yellow";
 var selectors = document.getElementsByClassName("color-selector");
 
 
@@ -51,9 +51,21 @@ var touchEndFunction = function(e) {
   isMouseDown = false;
 };
 
-var changeColorOnCLick = function(id) {
-  var color = document.getElementById(id).style.backgroundColor;
-  drawColor = color;
+var changeColorSelectBorder = function(id) {
+  for (var i=0; i<selectors.length; i++) {
+    selectors[i].style.borderColor = "transparent";
+  }
+  document.getElementById(id).style.borderColor = "black";
+};
+
+var hoverStateInOut = function(el, InOrOut) {
+  if (InOrOut === true) {
+    el.style.width = "53px";
+    el.style.height = "53px";
+  } else {
+    el.style.width = "50px";
+    el.style.height = "50px";
+  }
 };
 
 // mouse listeners
@@ -66,7 +78,18 @@ document.body.addEventListener("touchend", touchEndFunction);
 document.body.addEventListener("touchmove", touchMoveFunction);
 
 // more mouse listeners for interface
-document.getElementById("yellow-button").addEventListener("click", function() {drawColor = "yellow";});
-document.getElementById("blue-button").addEventListener("click", function() {drawColor = "blue";});
-document.getElementById("orange-button").addEventListener("click", function() {drawColor = "orange";});
-document.getElementById("red-button").addEventListener("click", function() {drawColor = "red";});
+document.getElementById("yellow-button").addEventListener("click", function() {drawColor = "yellow"; changeColorSelectBorder("yellow-button");});
+document.getElementById("yellow-button").addEventListener("mouseover", function() {hoverStateInOut(this, true);});
+document.getElementById("yellow-button").addEventListener("mouseout", function() {hoverStateInOut(this, false);});
+
+document.getElementById("blue-button").addEventListener("click", function() {drawColor = "blue"; changeColorSelectBorder("blue-button");});
+document.getElementById("blue-button").addEventListener("mouseover", function() {hoverStateInOut(this, true);});
+document.getElementById("blue-button").addEventListener("mouseout", function() {hoverStateInOut(this, false);});
+
+document.getElementById("orange-button").addEventListener("click", function() {drawColor = "orange"; changeColorSelectBorder("orange-button");});
+document.getElementById("orange-button").addEventListener("mouseover", function() {hoverStateInOut(this, true);});
+document.getElementById("orange-button").addEventListener("mouseout", function() {hoverStateInOut(this, false);});
+
+document.getElementById("red-button").addEventListener("click", function() {drawColor = "red"; changeColorSelectBorder("red-button");});
+document.getElementById("red-button").addEventListener("mouseover", function() {hoverStateInOut(this, true);});
+document.getElementById("red-button").addEventListener("mouseout", function() {hoverStateInOut(this, false);});
