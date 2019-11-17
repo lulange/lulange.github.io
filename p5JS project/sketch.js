49addcdf0e4344c155d13245af5437a49b34991c
lulange <53273617+lulange@users.noreply.github.com>
2019-11-17 12:42:01 -0500

    Add files via upload

---
+++ p5JS project/sketch.js
@@ -0,0 +1,17 @@
+let house;
+
+function preload() {
+  // Load model with normalise parameter set to true
+  house = loadModel('house.stl', true);
+}
+
+function setup() {
+  createCanvas(100, 100, WEBGL);
+}
+
+function draw() {
+  background(200);
+  scale(0.4); // Scaled to make model fit into canvas
+  normalMaterial(); // For effect
+  model(teapot);
+}
