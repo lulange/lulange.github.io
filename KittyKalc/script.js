c3055c90ee80fa190d205b460e12a35777d6a6cb
lulange <53273617+lulange@users.noreply.github.com>
2019-12-17 20:26:15 -0500

    Add files via upload

--- KittyKalc/script.js
+++ KittyKalc/script.js
@@ -1,1 +1,27 @@
+document.body.style.width = window.innerWidth - (window.innerWidth*0.02) + "px";
 
+var updateKittyValue = function() {
+  var players = parseFloat(document.getElementById("players-option").value);
+  var cardsOut = 0;
+  if (document.getElementById("checkbox-for-2").checked) {
+    cardsOut += 4;
+  }
+  if (document.getElementById("checkbox-for-3").checked) {
+    cardsOut += 4;
+  }
+  if (document.getElementById("checkbox-for-4").checked) {
+    cardsOut += 4;
+  }
+  if (document.getElementById("checkbox-for-6").checked) {
+    cardsOut += 4;
+  }
+  var cardsInKitty = (57-cardsOut) % players;
+  var cardsInHand = (57-cardsOut-cardsInKitty)/players;
+  var percentOfHand = Math.round((cardsInKitty/cardsInHand*10000))/100;
+  document.getElementById("cards-in-kitty").innerHTML = cardsInKitty;
+  document.getElementById("cards-in-hand").innerHTML = cardsInHand;
+  document.getElementById("percent-of-hand").innerHTML = percentOfHand;
+};
+
+updateKittyValue();
+window.setInterval(updateKittyValue, 10);
