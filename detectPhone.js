9c74080bcb0c1f8ae10b354922acd3fbf7a64dd7
lulange <53273617+lulange@users.noreply.github.com>
2020-08-15 09:45:02 -0400

    Add files via upload

--- detectPhone.js
+++ detectPhone.js
@@ -60,3 +60,36 @@ window.addEventListener("resize", function(e) {
     }
   }
 });
+
+document.addEventListener("resize" function() {
+  if (window.innerWidth < window.innerHeight) {
+    desktopChangelog.style.display = "none";
+    desktopOtherstuff.style.display = "none";
+
+    header.style.display = "block";
+    header.style.padding = "2%";
+
+    altFlex.style.width = "100%";
+    altFlex.style.display = "flex";
+  } else {
+    const altFlex = document.getElementById("alt-flex");
+    altFlex.style.display = "none";
+
+    desktopChangelog.style.display = "block";
+    desktopOtherstuff.style.display = "block";
+
+    header.style.display = "";
+    header.style.padding = "";
+  }
+
+
+  if (footers[0] !== undefined) {
+    footers[0].style.position = "absolute";
+    footers[0].style.right = "0px";
+    if (window.innerHeight > document.body.scrollHeight + 80) {
+      footers[0].style.top = window.innerHeight - 80 + "px";
+    } else {
+      footers[0].style.top = document.height - 80 + "px";
+    }
+  }
+});
