2953b31c159dc734acfe6291d3e63bb3eda9eacb
lulange <lucas@langes.us>
2020-11-03 21:51:42 -0500

    Update gameScene.js

--- projects/mazeGame/scenes/gameScene.js
+++ projects/mazeGame/scenes/gameScene.js
@@ -8,8 +8,21 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
+    let coinGraphic = this.add.graphics({x: 0, y: 0});
+    coinGraphic.fillStyle(0xffd700, 1.0);
+    coinGraphic.fillCircle(12, 12, 10);
+    coinGraphic.fillStyle(0x000000, 1.0);
+    coinGraphic.fillCircle(12, 12, 4);
+    coinGraphic.lineStyle(2, 0xdaa520, 1.0);
+    coinGraphic.strokeCircle(12, 12, 10);
+    coinGraphic.generateTexture("coin", 24, 24);
+    gameState.coin = this.add.sprite(100, 100, "coin");
+    gameState.coin.setDepth(1);
+
+
+
     let playerGraphics = this.add.graphics({x: 0, y: 0});
-    playerGraphics.fillStyle(0x4666FF, 1.0);
+    playerGraphics.fillStyle(0x23e000, 1.0);
     playerGraphics.fillRoundedRect(0, 0, 10, 10, 5);
     playerGraphics.generateTexture("player", 10, 10);
     playerGraphics.fillStyle(0x000000, 1.0);
