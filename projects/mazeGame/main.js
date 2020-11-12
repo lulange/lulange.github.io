b997565b82c0da99a9537fec7bd95fda2dc6e78b
lulange <lucas@langes.us>
2020-11-11 21:00:58 -0500

    work on the maze game

--- projects/mazeGame/main.js
+++ projects/mazeGame/main.js
@@ -1,21 +1,51 @@
+/****************
+* GAME AREA SETUP
+****************/
+// global canvas variable
+const canvas = document.createElement("CANVAS");
+canvas.width = 900;
+canvas.height = 600;
+// global canvasParent variable
+const canvasParent = document.getElementById("canvas-wrapper");
+canvasParent.appendChild(canvas);
+// global context variable
+const ctx = canvas.getContext("2d");
+// global utility variable used to hold values that must be present from scene to scene
 const gameState = {
 
 };
 
-const config = {
-	type: Phaser.AUTO,
-	width: 900,
-	height: 600,
-	backgroundColor: "b6eaff",
-  parent: "canvas-wrapper",
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 0 },
-			enableBody: true,
-		}
-	},
-	scene: [GameScene]
-};
+// global scene/game manager
+class Game {
+	constructor() {
+		this.currScene = null;
+		this.scenes = [];
+		this.sceneInterval = null;
+	}
 
-let game = new Phaser.Game(config);
+	createScene(key, startupFunction, loopFunction) {
+		this.scenes.push({key: key, startupFunction: startupFunction, loopFunction: loopFunction});
+	}
+
+	runScene(key, data, speed) {
+		if (speed === undefined || speed === null) {
+			speed = 30;
+		}
+		if (this.sceneInterval !== null) {
+			window.clearInterval(this.sceneInterval);
+			this.sceneInterval = null;
+		}
+		const isCorrectKey = (scene) => {return scene.key === key};
+		const sceneIndex = this.scenes.findIndex(isCorrectKey);
+		let scene = this.scenes[sceneIndex];
+		this.currScene = scene.key;
+		scene.startupFunction(data);
+		window.setInterval(function() {scene.loopFunction(data);}, speed);
+	}
+
+	deleteScene(key) {
+		const isCorrectKey = (scene) => {return scene.key === key};
+		const sceneIndex = this.scenes.findIndex(isCorrectKey);
+		this.scenes.splice(sceneIndex, 1);
+	}
+}
