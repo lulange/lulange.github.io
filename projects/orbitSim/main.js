71ea233016c358025f3f12757440a2a8e1f715d4
lulange <lucas@langes.us>
2020-11-19 15:00:02 -0500

    various changes and improvements

--- projects/orbitSim/main.js
+++ projects/orbitSim/main.js
@@ -37,7 +37,7 @@ class Planet {
 	draw() {
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.width, 0, 2*Math.PI);
-		ctx.fillStyle = "#00FF00";
+		ctx.fillStyle = "#00FFFF";
 		ctx.fill();
 	}
 	update() {
@@ -58,7 +58,8 @@ for (let i=0; i<100; i++) {
 	// random:planets.push(new Planet(Math.random() * 8 + 2, Math.random()*300, 300, 0, Math.random()*4 + 2));
 	// descending size:planets.push(new Planet(8 - (i/100) * 8, 250, 300, 0, 4));
 	// descending velocity:planets.push(new Planet(8, 250, 300, 0, 5 - (i/100) * 5));
-	// both of the two above:planets.push(new Planet(8 - (i/100) * 8, 250, 300, 0, 5 - (i/100) * 5));
+	// both of the two above:
+	planets.push(new Planet(8 - (i/100) * 8, 250, 300, 0, 5 - (i/100) * 5));
 	// descending size and low start velocity:planets.push(new Planet(8 - (i/100) * 8, 250, 300, 0, 0.2));
 	// descending size and descending x coor with low start velocity:planets.push(new Planet(8 - (i/100) * 8, 300 - (i/100) * 200, 300, 0, 0.2));
 	// descending size:planets.push(new Planet(8, 300 - (i/100) * 200, 300, 0, 5));
@@ -67,6 +68,7 @@ for (let i=0; i<100; i++) {
 
 let drawBetweenPlanets = () => {
 	ctx.strokeStyle = "#00FF00";
+	ctx.lineWidth = 2;
 	for (let i=0; i<planets.length-1; i++) {
 		ctx.beginPath();
 		ctx.moveTo(planets[i].x, planets[i].y);
@@ -77,10 +79,10 @@ let drawBetweenPlanets = () => {
 let draw = () => {
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	//sun.draw();
+	sun.draw();
 
 	for (let i=0; i<planets.length; i++) {
-		//planets[i].draw();
+		planets[i].draw();
 		planets[i].gravitise();
 		planets[i].update();
 	}
