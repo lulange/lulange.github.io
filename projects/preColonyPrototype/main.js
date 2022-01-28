f306ceb455b1db636f5a6417eed9af2566e5e631
lulange <lucas@langes.us>
2022-01-28 15:13:39 -0500

    f

--- projects/preColonyPrototype/main.js
+++ projects/preColonyPrototype/main.js
@@ -1,5 +1,8 @@
 // global variable setup
 let gameScene = 0; // decider for rendering and listeners
+let selectedMaterial = "metal";
+let currentCreation;
+let shapes = [];
 // module aliases
 let Engine = Matter.Engine,
     Runner = Matter.Runner,
@@ -9,6 +12,17 @@ let Engine = Matter.Engine,
 // create an engine
 let engine = Engine.create();
 
+// create runner
+let runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+// just a quick space saver
+let renderRect = function(rect) {
+  quad(rect.vertices["0"].x, rect.vertices["0"].y, rect.vertices["1"].x, rect.vertices["1"].y, rect.vertices["2"].x, rect.vertices["2"].y, rect.vertices["3"].x, rect.vertices["3"].y);
+};
+
 // create two boxes and a ground
 let boxA = Bodies.rectangle(400, 200, 80, 80);
 let boxB = Bodies.rectangle(450, 50, 80, 80);
@@ -16,12 +30,6 @@ let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground]);
-
-// create runner
-let runner = Runner.create();
-
-// run the engine
-Runner.run(runner, engine);
 
 
 // setup p5.js stuff
@@ -38,9 +46,50 @@ function draw() {
 		case 0:
 			background(255, 255, 255);
 			fill(0, 0, 0);
-			quad(boxA.vertices["0"].x, boxA.vertices["0"].y, boxA.vertices["1"].x, boxA.vertices["1"].y, boxA.vertices["2"].x, boxA.vertices["2"].y, boxA.vertices["3"].x, boxA.vertices["3"].y);
-			quad(boxB.vertices["0"].x, boxB.vertices["0"].y, boxB.vertices["1"].x, boxB.vertices["1"].y, boxB.vertices["2"].x, boxB.vertices["2"].y, boxB.vertices["3"].x, boxB.vertices["3"].y);
-
+			renderRect(boxA);
+      renderRect(boxB);
+      fill(100, 255, 100);
+      renderRect(ground);
 			break;
 	}
+}
+
+// setup shape creation for beams
+function mousePressed(e) {
+  switch(gameScene) {
+    case 0:
+      currentCreation = {
+        x: e.x,
+        y: e.y,
+        width: 24,
+        height: 0
+      };
+      break;
+  }
+}
+
+function mouseMoved(e) {
+  switch(gameScene) {
+    case 0:
+      currentCreation = {
+        x: e.x,
+        y: e.y,
+        width: 24,
+        height: 0
+      };
+      break;
+  }
+}
+
+function mouseReleased(e) {
+  switch(gameScene) {
+    case 0:
+      currentCreation = {
+        x: e.x,
+        y: e.y,
+        width: 24,
+        height: 0
+      };
+      break;
+  }
 }
