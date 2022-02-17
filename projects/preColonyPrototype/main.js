1cef414ec0711311eb94c2bf86ac270af3853251
lulange <lucas@langes.us>
2022-02-17 18:31:08 -0500

    fixed beam bug for pre-colony prototype

--- projects/preColonyPrototype/main.js
+++ projects/preColonyPrototype/main.js
@@ -1,8 +1,27 @@
-// global variable setup
+/**********************
+ * MY GLOBAL VARIABLES
+**********************/
+
 let gameScene = 0; // decider for rendering and listeners
+let currentEntity = 0; // which vehicle you have selected to control and edit
+let entities = [{ // variable to keep track of created vehicles and stuff
+  name: "vehicle1",
+  parts: [],
+  isEnabled: false,
+}];
+
+// for editscene (0) only
+let selectedItem = "beam"; // to keep track of the current item being placed
 let selectedMaterial = "metal";
 let currentCreation = {height: 0};
-let shapes = [];
+
+let adjMouseX; // globally declared, but set in the draw loop
+let adjMouseY;
+
+
+/**********************
+ * MATTER.JS SETUP
+**********************/
 // module aliases
 let Engine = Matter.Engine,
     Runner = Matter.Runner,
@@ -18,20 +37,27 @@ let runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 
+
+/*********************
+ * GLOBAL FUNCTIONS
+*********************/
 // just a quick space saver
 let renderRect = function(rect) {
   quad(rect.vertices["0"].x, rect.vertices["0"].y, rect.vertices["1"].x, rect.vertices["1"].y, rect.vertices["2"].x, rect.vertices["2"].y, rect.vertices["3"].x, rect.vertices["3"].y);
 };
 
-// create two boxes and a ground
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(450, 50, 80, 80);
+/**************************
+  * TEMPORARY GLOBAL STUFF
+**************************/
+// create ground for temporary use
 let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [ground]);
 
-
+/****************
+ * P5.JS SETUP
+****************/
 // setup p5.js stuff
 function setup() {
 	// setup canvas in the correct placement
@@ -40,20 +66,23 @@ function setup() {
 	document.body.getElementsByTagName("main")[0].remove();
 }
 
-
+/**********************
+ * MAIN ANIMATION LOOP
+**********************/
 function draw() {
+  adjMouseX = mouseX - 10;
+  adjMouseY = mouseY - 10;
 	switch(gameScene) {
-		case 0:
+		case 0: // editing scene
+
+      // white background
 			background(255, 255, 255);
-			fill(0, 0, 0);
-			renderRect(boxA);
-      renderRect(boxB);
+
       fill(30, 30, 30);
       if (currentCreation.height > 5) {
-        let currCreationXBoost = currentCreation.width*Math.cos(Math.PI / 2 + currentCreation.angle);
-        console.log(currCreationXBoost);
-        let currCreationYBoost = currentCreation.width*Math.sin(Math.PI / 2 + currentCreation.angle);
-        quad(currentCreation.x1 + currCreationXBoost, currentCreation.y1 + currCreationYBoost, currentCreation.x2 + currCreationXBoost, currentCreation.y2 + currCreationYBoost, currentCreation.x2, currentCreation.y2, currentCreation.x1, currentCreation.y1);
+        let currCreationXBoost = currentCreation.width*Math.cos(-Math.PI/2 + currentCreation.angle);
+        let currCreationYBoost = currentCreation.width*Math.sin(-Math.PI/2 + currentCreation.angle);
+        quad(currentCreation.x1 + currCreationXBoost/2, currentCreation.y1 + currCreationYBoost/2, currentCreation.x2 + currCreationXBoost/2, currentCreation.y2 + currCreationYBoost/2, currentCreation.x2 - currCreationXBoost/2, currentCreation.y2 - currCreationYBoost/2, currentCreation.x1 - currCreationXBoost/2, currentCreation.y1 - currCreationYBoost/2);
       }
       fill(100, 255, 100);
       renderRect(ground);
@@ -61,13 +90,16 @@ function draw() {
 	}
 }
 
-// setup shape creation for beams
+
+/***********************
+ * EVENT LISTENERS
+***********************/
 function mousePressed(e) {
   switch(gameScene) {
     case 0:
       currentCreation = {
-        x1: mouseX,
-        y1: mouseY,
+        x1: adjMouseX,
+        y1: adjMouseY,
         width: 16,
         height: 0
       };
@@ -78,10 +110,11 @@ function mousePressed(e) {
 function mouseDragged(e) {
   switch(gameScene) {
     case 0:
-      currentCreation.angle = Math.atan((mouseY - currentCreation.y1) / (mouseX - currentCreation.x1));
-      currentCreation.x2 = mouseX;
-      currentCreation.y2 = mouseY;
-      currentCreation.height = Math.sqrt((mouseY - currentCreation.y1)**2 + (mouseX - currentCreation.x1)**2);
+      currentCreation.angle = Math.atan((adjMouseY - currentCreation.y1) / (adjMouseX - currentCreation.x1));
+
+      currentCreation.x2 = adjMouseX;
+      currentCreation.y2 = adjMouseY;
+      currentCreation.height = Math.sqrt((adjMouseY - currentCreation.y1)**2 + (adjMouseX - currentCreation.x1)**2);
       break;
   }
 }
@@ -90,7 +123,7 @@ function mouseReleased(e) {
   switch(gameScene) {
     case 0:
       if (currentCreation.height < 5) {
-        //let beamBody = Bodies.polygon();
+
       }
       break;
   }
