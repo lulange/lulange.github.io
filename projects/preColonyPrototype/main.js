db7c355bb094d9925c94f3a7f610fa04e62db199
lulange <lucas@langes.us>
2023-09-26 21:03:12 -0400

    Deleted unfinished projects

--- projects/preColonyPrototype/main.js
+++
@@ -1,6 +0,0 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-ctx.fillstyle = "rgb(255, 0, 0)";
-ctx.strokestyle = "white";
-ctx.fillRect(10, 10, 30, 30);
